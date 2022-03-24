c4416da42d4aa637398da04296cdbf2f4df72983
Thomas Mével <thomas.mevel@protonmail.com>
2022-03-24 18:10:35 +0100

    VPS IP instead of localhost

--- rpi/streaming.sh
+++ rpi/streaming.sh
@@ -1,1 +1,1 @@
-ffmpeg -re -i /dev/video0 -c:v libx264 -preset veryfast -tune zerolatency -c:a aac -ar 44100 -pix_fmt yuv420p -f flv rtmp://localhost/live/test
+ffmpeg -re -i /dev/video0 -c:v libx264 -preset veryfast -tune zerolatency -c:a aac -ar 44100 -pix_fmt yuv420p -f flv rtmp://195.14.189.82/live/test
