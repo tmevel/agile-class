35d5770738a8450f397b4ce0dd1e2f982ea56b6d
Thomas Mével <thomas.mevel@protonmail.com>
2022-05-15 20:09:56 +0200

    video backend

--- backend/app.js
+++ backend/app.js
@@ -69,11 +69,36 @@ app.post('/upload', function(req, res) {
     }
 });
 
-app.get('/video', (req, res, next) => {
-    res.setHeader('Content-Type', 'video/mp4');
-
-    res.status(200).sendFile(__dirname+req.query.file);
-})
+app.get('/video', function(req, res) {
+    const path = __dirname+req.query.file
+    const stat = fs.statSync(path)
+    const fileSize = stat.size
+    const range = req.headers.range
+    if (range) {
+      const parts = range.replace(/bytes=/, "").split("-")
+      const start = parseInt(parts[0], 10)
+      const end = parts[1] 
+        ? parseInt(parts[1], 10)
+        : fileSize-1
+      const chunksize = (end-start)+1
+      const file = fs.createReadStream(path, {start, end})
+      const head = {
+        'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+        'Accept-Ranges': 'bytes',
+        'Content-Length': chunksize,
+        'Content-Type': 'video/mp4',
+      }
+      res.writeHead(206, head);
+      file.pipe(res);
+    } else {
+      const head = {
+        'Content-Length': fileSize,
+        'Content-Type': 'video/mp4',
+      }
+      res.writeHead(200, head)
+      fs.createReadStream(path).pipe(res)
+    }
+});
 
 // Activation reports
 
